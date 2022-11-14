14e8270d64aca903564e5897461179069c11fa3d
unknown <joshuaobrien28@yahoo.com>
2022-11-14 14:32:44 -0500

    added JS test, coded some standard HTML to connect style and JS script

---
+++ assets/js/Build-work.js
@@ -0,0 +1,17 @@
+// Variables for full overview of test
+var testQuestions = 0;
+var time = testQuestions.length * 10;
+var timerId;
+
+// Variables to callout unique elements 
+
+var testQuestionsEl = document.getElementById('questions');
+var timeEl = document.getElementById('time');
+var selection = document.getElementById('selection');
+var submitBtn = document.getElementById('submit');
+var beginBtn = document.getElementById('begin');
+var feeedBackEl = document.getElementById('feedback');
+
+function beginQuiz() {
+
+}
