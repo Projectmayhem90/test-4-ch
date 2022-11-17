122603b9e3f2babc7fdf83b340ae6d071bfcff7f
unknown <joshuaobrien28@yahoo.com>
2022-11-16 22:47:47 -0500

    Added questions and some features to JS

--- assets/js/Build-work.js
+++ assets/js/Build-work.js
@@ -1,17 +1,68 @@
 // Variables for full overview of test
 var testQuestions = 0;
-var time = testQuestions.length * 10;
-var timerId;
+var time = 100;
+var timerMetric;
+var testQuestionsCorrect = 0;
 
 // Variables to callout unique elements 
 
-var testQuestionsEl = document.getElementById('questions');
-var timeEl = document.getElementById('time');
-var selection = document.getElementById('selection');
-var submitBtn = document.getElementById('submit');
-var beginBtn = document.getElementById('begin');
-var feeedBackEl = document.getElementById('feedback');
+var selectionEl = document.getElementById('selection');
+var answersEl = document.getElementById('answers');
+var timeremainingEl = document.getElementById('timeRemaining');
+var questionEl = document.getElementById('questions');
 
-function beginQuiz() {
+// Buttons Var
 
+var startbutton = document.getElementById("startbutton");
+
+function startQuiz() {
+    console.log("startQuiz run");
+    var welcomeScreenEl = document.getElementById("welcomeScreen");
+    welcomeScreenEl.setAttribute("class", "hidden");
+
+    var questionScreenEl = document.getElementById("questionScreen");
+    questionScreenEl.removeAttribute("class");
+
+    timerMetric = setInterval(timerCoundown, 1000);
+    timeLeftEl.textContent = time;
+
+    displayQuestion();
+};
+
+function displayQuestion() {
+    var questionData = questions[numericValue];
+
+    var questionEl = document.getElementById('question')
+    questionEl.textContent = questionData.question;
 }
+
+startbutton.addEventListener("click, startQuiz);
+selectionEl.addEventListener("click", SelectOption);
+
+var questions = [
+    {
+        question: "Why do we put the JS script at the bottom of the HTML? "
+        selection: ["It looks cool", "Chris told me to put it there", "It is programmed that way", "Its due to how web browsers load a HTML file"]
+        answer: "Its due to how web browsers load a HTML file",
+    },
+    {
+        question: "What do Booleans do in JavaScript?"
+        selection: ["A simple way to know yes or no via true or false", "It scares the other code into working", "Its a function to call out a console log", "It shows the format of JS"]
+        answer: "A simple way to know yes or no via true or false",
+    }, 
+    {
+        question: "What would you use an array for in JavaScript?"
+        selection: ["You should use rrays when you want the element names to be numbers", "Its a mathmatic value to answer true or false", "I wish i knew", "Its a quick was to find the rays of the webpage"]
+        answer: "You should use rrays when you want the element names to be numbers",
+    },
+    {
+        question: "what is JavaScript used for? "
+        selection: ["JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else", "JavaScript is the language for describing the presentation of Web pages, including colors, layout, and fonts", "JavaScript  is the language for describing the structure of Web pages", "JavaScript is used because it was created during the big bang"]
+        answer: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else",
+    },
+    { 
+        question: "Who invented JavaScript?"
+        selection: ["Dana Boston", "Albert Einstien", "Dr.Seuss", "Brendan Eich"]
+        answer: "Brendan Eich",
+    }
+]
