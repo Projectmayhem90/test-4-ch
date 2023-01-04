f0c67ca39ce51fb51135d30121be1a548d8a9cbf
NatJoshYoyo <joshuaobrien28@yahoo.com>
2023-01-03 22:44:42 -0500

    Added ReadME

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # test-4-ch
 Repo for 4th OSU bootcamp challenge 
+
+# Home Page / Coding Quiz
+
+![Webpage ](./assets/Images/Test%20Pic%201.png)
+
+# Questions Page / Coding Quiz
+
+![Webpage ](./assets/Images/Test%20Pic%202.png
+
+## Description 
+
+A Coding Quiz to test your Java Knowledge
+
+
+
+## Live Website / Application 
+https://projectmayhem90.github.io/test-4-ch/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,77 @@
-// Variables for full overview of test
+// G Variables
 var questionNumber = 0;
-var time = 100;
+var time = 90;
 var timeVar;
 var questionsCorrect = 0;
 var userScore;
 
-// Variables to callout unique elements 
-
+//Page Elements
 var optionsEl = document.getElementById("options");
+var timeLeftEl = document.getElementById("timeLeft");
 var responseEl = document.getElementById("response");
-var timeLeftEl = document.getElementById('timeLeft');
-var returnHomeEl = document.getElementById('returnHome');
+var returnHomeEl = document.getElementById("returnHome");
 
-// Buttons Var
-
-var startButton = document.getElementById("startButton");
-var viewHighBtn = document.getElementById("viewHigh");
-
-//JS for new pages
-
-var welcomeScreenEl = document.getElementById ("welcomeScreen");
+// Variable new page
+var welcomeScreenEl = document.getElementById("welcomeScreen");
 var questionScreenEl = document.getElementById("questionScreen");
 var scoreScreenEl = document.getElementById("scoreScreen");
 var highScoreScreenEl = document.getElementById("highScoreScreen");
 
-//Global
+// Buttons
+var startButton = document.getElementById("startButton");
+var viewHighBtn = document.getElementById("viewHigh");
 
-startButton.addEventListener("click", "startQuiz");
-responseEl.addEventListener("class", "returnHome" )
-viewHighBtn.addEventListener("click", "highScoreScreen");
-optionsEl.addEventListener("click", "chooseOption");
-
-//Function for quiz
+startButton.addEventListener("click", startQuiz);
+returnHomeEl.addEventListener("click", returnHome);
+viewHighBtn.addEventListener("click", highScoreScreen);
+optionsEl.addEventListener("click", chooseOption);
 
 function startQuiz() {
 
     welcomeScreenEl.setAttribute("class", "hidden");
-
     returnHomeEl.removeAttribute("class");
-
     questionScreenEl.removeAttribute("class");
 
-    //Timer
-
-    timeVar = setInterval(timerCoundown, 1000);
-    timeLeftEl.textContent = time;
-
-    displayQuestion();
-
-    var questionScreenEl = document.getElementById("questionScreen");
-    questionScreenEl.removeAttribute("class");
-
-    timerMetric = setInterval(timerCoundown, 1000);
+    // Timer
+    timeVar = setInterval(timerCountdown, 1000);
     timeLeftEl.textContent = time;
 
     displayQuestion();
 };
 
-function timerCoundown() {
+// Timer F
+function timerCountdown() {
     time--;
 
-    timeRemainingEl.textContent = time;
+    timeLeftEl.textContent = time;
+
+    
     if (time <= 0) {
-        scoreScreenEl();
+        scoreScreen();
     };
 }
 
+// Multiple choice
 function displayQuestion() {
+   
     var questionInfo = questions[questionNumber];
 
+    
     var questionEl = document.getElementById("question");
     questionEl.textContent = questionInfo.question;
+
+   
     optionsEl.innerHTML = '';
 
+   
     for (var i = 0; i < questionInfo.options.length; i++) {
         var option = questionInfo.options[i];
         var optionButton = document.createElement("button");
         optionButton.setAttribute("class", "option");
         optionButton.setAttribute("value", option);
-        optionButton.textContent = i + 1 `. ${option}`;
+
+        optionButton.textContent = i + 1 + ". " + option;
+
         optionsEl.appendChild(optionButton);
     };
 };
@@ -90,29 +83,32 @@ function chooseOption(event) {
         return;
     };
 
+    // If selected option is wrong
     if (buttonEl.value !== questions[questionNumber].answer) {
-        time -= 15;
+        time -= 10;
+
         if (time <= 0) {
             time = 0;
         }
-
         responseEl.setAttribute("class", "incorrect");
-        responseEl.textContent = "Incorrect";
+        responseEl.textContent = "Incorrect!";
+
     } else {
         questionsCorrect++;
 
         responseEl.setAttribute("class", "correct");
-        responseEl.textContent("class", "Correct");
+        responseEl.textContent = "Correct!";
     };
+
 
     setTimeout(function () {
         responseEl.setAttribute("class", "hidden");
     }, 1000);
 
     questionNumber++;
-
     userScore = time * questionsCorrect;
-    if (time <= 0 || questionNumber === question.length) {
+
+    if (time <= 0 || questionNumber === questions.length) {
         scoreScreen();
     } else {
         displayQuestion();
@@ -120,11 +116,9 @@ function chooseOption(event) {
 };
 
 function scoreScreen() {
-
     clearInterval(timeVar);
 
-    questionScreenEl.removeAttribute("class", "hidden");
-
+    questionScreenEl.setAttribute("class", "hidden");
     scoreScreenEl.removeAttribute("class");
 
     var scoreEl = document.getElementById("score");
@@ -135,29 +129,28 @@ function scoreScreen() {
 };
 
 function saveScore() {
+
     var userNameEl = document.getElementById("userName");
     var userName = userNameEl.value;
-
     var scores = JSON.parse(window.localStorage.getItem("scores")) || [];
     var userInfo = {
         userName: userName,
         userScore: userScore,
     };
 
-    scores.push(userInfor);
+    scores.push(userInfo);
     window.localStorage.setItem("scores", JSON.stringify(scores));
 
     highScoreScreen();
 }
 
 function highScoreScreen() {
+
     welcomeScreenEl.setAttribute("class", "hidden");
     questionScreenEl.setAttribute("class", "hidden");
     scoreScreenEl.setAttribute("class", "hidden");
-
     highScoreScreenEl.removeAttribute("class");
     viewHighBtn.setAttribute("class", "hidden");
-
     returnHomeEl.removeAttribute("class")
 
     var scores = JSON.parse(window.localStorage.getItem("scores")) || [];
@@ -170,17 +163,17 @@ function highScoreScreen() {
         var scoreLi = document.createElement("li");
         scoreLi.textContent = scores[i].userName + ": " + scores[i].userScore;
 
-        var score01 = document.getElementById("score01");
-        score01.appendChild(scoreLi);
+        var scoreOl = document.getElementById("scoreOl");
+        scoreOl.appendChild(scoreLi);
     }
 
-    var clearScoreBtn = document.getElementById("clearScores");
+    var clearScoresBtn = document.getElementById("clearScores");
     clearScoresBtn.addEventListener("click", clearScores);
 }
 
 function clearScores() {
     window.localStorage.removeItem('scores');
-    returnHomeEl();
+    returnHome();
 }
 
 function returnHome() {
@@ -190,27 +183,32 @@ function returnHome() {
 var questions = [
     {
         question: "Why do we put the JS script at the bottom of the HTML?",
-        selection: ["It looks cool", "Chris told me to put it there", "It is programmed that way", "Its due to how web browsers load a HTML file"],
+        options: ["It looks cool", "Chris told me to put it there", "It is programmed that way", "Its due to how web browsers load a HTML file"],
         answer: "Its due to how web browsers load a HTML file",
     },
     {
         question: "What do Booleans do in JavaScript?",
-        selection: ["A simple way to know yes or no via true or false", "It scares the other code into working", "Its a function to call out a console log", "It shows the format of JS"],
+        options: ["A simple way to know yes or no via true or false", "It scares the other code into working", "Its a function to call out a console log", "It shows the format of JS"],
         answer: "A simple way to know yes or no via true or false",
     },
     {
         question: "What would you use an array for in JavaScript?",
-        selection: ["You should use rrays when you want the element names to be numbers", "Its a mathmatic value to answer true or false", "I wish i knew", "Its a quick was to find the rays of the webpage"],
+        options: ["You should use rrays when you want the element names to be numbers", "Its a mathmatic value to answer true or false", "I wish i knew", "Its a quick was to find the rays of the webpage"],
         answer: "You should use rrays when you want the element names to be numbers",
     },
     {
         question: "what is JavaScript used for?",
-        selection: ["JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else", "JavaScript is the language for describing the presentation of Web pages, including colors, layout, and fonts", "JavaScript  is the language for describing the structure of Web pages", "JavaScript is used because it was created during the big bang"],
+        options: ["JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else", "JavaScript is the language for describing the presentation of Web pages, including colors, layout, and fonts", "JavaScript  is the language for describing the structure of Web pages", "JavaScript is used because it was created during the big bang"],
         answer: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else",
     },
     {
+        question: "Which is the following is used when declaring an object?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "{ }",
+    },
+    {
         question: "Who invented JavaScript?",
-        selection: ["Dana Boston", "Albert Einstien", "Dr.Seuss", "Brendan Eich"],
-        answer: "Brendan Eich",
+        options: ["Jakob Anderson", "Albert Einstien", "Dr.Seuss", "Brendan Eich"],
+        answer: "Branden Eich",
     }
 ]
